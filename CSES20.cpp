45d6b2ff1fbeda7e7a3991dcce4b81a6be6f53f9
Ashoo Pandey <113572517+Pandeyashoo@users.noreply.github.com>
2023-01-18 23:24:36 +0530

    18 jan
    SEARCHING AND SORTING

---
+++ CSES20.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n;
+   cin>>n;
+   int arr[n];
+   for(int i=0;i<n; i++){
+       cin>>arr[i];
+   }
+   set<int>s;
+   for(int i=0; i<n; i++){
+       s.insert(arr[i]);
+   }
+   int ans=s.size();
+   cout<<ans<<endl;
+}
